a5a1cb2d7f50a7822af4367ab5df0286255bdc27
shpvsk <mshpvsk@yandex.ru>
2022-08-24 20:37:39 +0300

    feat: add nav sticky

--- app/js/index.js
+++ app/js/index.js
@@ -5,14 +5,3 @@ import './components/burger-menu'
 import './components/nav-callback-btn'
 import './components/slider'
 
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-window.initMap = initMap;
