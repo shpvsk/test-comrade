3cc392184010ae8e012f6fafc2762eeeaf102442
shpvsk <mshpvsk@yandex.ru>
2022-08-24 19:25:56 +0300

    feat: add map section

--- app/js/index.js
+++ app/js/index.js
@@ -4,3 +4,15 @@ import './components/lazy-load'
 import './components/burger-menu'
 import './components/nav-callback-btn'
 import './components/slider'
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
